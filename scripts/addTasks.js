ef29b44d2f5a8a82ab1b371d1338753e47c95567
Farhan Khan <farhankhan.devv@gmail.com>
2026-03-24 18:48:37 +0530

    Added HTML via DOM manipulation and made the add task button interactive..

--- scripts/addTasks.js
+++ scripts/addTasks.js
@@ -1,1 +1,32 @@
+document.querySelector('.js-add-task-btn').addEventListener('click', () => {
+  const task = document.querySelector('input').value;
+  const allTasksContainer = document.querySelector('.all-tasks-container');
 
+  const div = document.createElement('div');
+  div.className = "task-container";
+
+  const label = document.createElement('label');
+  label.className = "task-complete";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  const span = document.createElement('span');
+  span.className = "checkmark";
+
+  const p = document.createElement('p');
+  p.innerText = `${task}`
+
+  const button = document.createElement('button');
+  button.className = "delete-task";
+
+  //nesting 
+  div.appendChild(label);
+  label.append(checkbox, span);
+  div.append(p, button);
+  allTasksContainer.prepend(div);
+
+
+  document.querySelector('input').value = '';
+
+});
